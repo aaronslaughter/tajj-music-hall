92436600b0c9ebd35cdef6cffbf565e2d428af12
janemzwangnj <janemzwangnj@gmail.com>
2022-01-14 13:15:42 -0500

    itinial files for junction Model Users_Events

---
+++ migrations/20220114181338-create-users-events.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users_Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      event_id: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users_Events');
+  }
+};
---
+++ models/users_events.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users_Events extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Users_Events.init({
+    event_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Users_Events',
+  });
+  return Users_Events;
+};
