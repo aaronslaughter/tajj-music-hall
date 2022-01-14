36f1f3198ab7b2d20e838350e7d1ec429aa0cb6c
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-14 16:24:44 -0500

    replaced typo in variable name

--- client/src/services/BITTestServices.js
+++ client/src/services/BITTestServices.js
@@ -3,7 +3,7 @@ import { BITClient } from "./index";
 export const GetEvents = async () => {
   try {
     let example = 'Red Hot Chili Peppers'
-    const response = await BITClient.get(`/artists/${example}/events/?app_id=${process.env.REACT_APP_BIIT_API_KEY}`)
+    const response = await BITClient.get(`/artists/${example}/events/?app_id=${process.env.REACT_APP_BIT_API_KEY}`)
     return response.data
   } catch (error) {
     throw error
