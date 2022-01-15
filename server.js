dfa0e2d48ec4d941eda901b7c6c76757ba4fc852
Terra Smith <t_m@linuxmail.org>
2022-01-15 12:52:41 -0800

    modified server.js file

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const logger = require('morgan')
+const AuthRouter = require('./routes/AuthRouter')
 const AppRouter = require('./routes/AppRouter');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(logger('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }));
 app.use('/api', AppRouter);
+app.use('/auth', AuthRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
