91e5ab4d8ae9929b95a0fd5cd2932422de7ce729
janemzwangnj <janemzwangnj@gmail.com>
2022-01-14 23:16:13 -0500

    updated User routes and controller(CRUD works for users)

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,36 @@ const createNewUser = async (req, res) => {
   res.status(201).send(newUser);
 };
 
+const updateUser = async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const updatedUser = await User.update(
+    {
+      ...req.body,
+      updatedAt: new Date()
+    },
+    {
+      where: {
+        id: userId
+      },
+      returning: true
+    }
+  );
+  res.status(200).send(updatedUser);
+};
+
+const deleteUser = async (req, res) => {
+  const deletedId = parseInt(req.params.userId);
+  await User.destroy({
+    where: {
+      id: deletedId
+    }
+  });
+  res.status(200).send({ message: `Deleted user with id of ${deletedId}!` });
+};
+
 module.exports = {
   getAllUsers,
-  createNewUser
+  createNewUser,
+  updateUser,
+  deleteUser
 };
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/userController');
 
 router.get('/', controller.getAllUsers);
 router.post('/', controller.createNewUser);
-// router.put('/:userId', controller.updateUser);
-// router.delete('/:userId', controller.deleteUser);
+router.put('/:userId', controller.updateUser);
+router.delete('/:userId', controller.deleteUser);
 
 module.exports = router;
