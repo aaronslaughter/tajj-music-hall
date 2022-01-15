1583f402e59f287a1e910c33f00a3332bb4e319d
janemzwangnj <janemzwangnj@gmail.com>
2022-01-15 00:20:13 -0500

    added CRUD for events

--- routes/EventRouter.js
+++ routes/EventRouter.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 
+const controller = require('../controllers/eventController');
+
+router.get('/', controller.getAllEvents);
+router.post('/', controller.createNewEvent);
+router.put('/:eventId', controller.updateEvent);
+router.delete('/:eventId', controller.deleteEvent);
+
 module.exports = router;
