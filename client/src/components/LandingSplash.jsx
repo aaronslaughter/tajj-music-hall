d4d3e013d5b975575e65a9f0b5b17807841c770d
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-15 20:40:21 -0500

    added first pass on the landing splash image

---
+++ client/src/components/LandingSplash.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingSplash = () => {
+  return (
+    <div>
+      WHAT EVENT ARE YOU GOING TO NEXT?
+      {/*TODO: add splash image*/}
+    </div>
+  )
+}
+
+export default LandingSplash
