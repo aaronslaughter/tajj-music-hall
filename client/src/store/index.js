922cd9b0beaeadf863f33cbc3ebb6cc76b7fe9af
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-16 14:55:20 -0500

    combined SearchReducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import DiscoverReducer from './reducers/DiscoverReducer'
+import SearchReducer from "./reducers/SearchReducer"
 import BITTestReducer from './reducers/BITTestReducer'
 
 const store = createStore(
   combineReducers({
     discoverState: DiscoverReducer,
+    searchState: SearchReducer,
     bitState: BITTestReducer
   }),
   composeWithDevTools(applyMiddleware(thunk))
