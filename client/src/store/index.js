1e7e9ba473093b4e01e014f10c6ee8bcb650b03d
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-17 16:04:03 -0500

    added eventState

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,11 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import DiscoverReducer from './reducers/DiscoverReducer'
 import SearchReducer from "./reducers/SearchReducer"
+import EventReducer from './reducers/EventReducer'
 
 const store = createStore(
   combineReducers({
     discoverState: DiscoverReducer,
-    searchState: SearchReducer
+    searchState: SearchReducer,
+    eventState: EventReducer
   }),
   composeWithDevTools(applyMiddleware(thunk))
 )
