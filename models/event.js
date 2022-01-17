19ba4922d1c6c69ef26153dd872ad5f8f8dd5455
janemzwangnj <janemzwangnj@gmail.com>
2022-01-16 20:08:44 -0500

    updated Event and User model

--- models/event.js
+++ models/event.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Event.hasMany(models.Comment, { foreignKey: 'event_id' });
-      Event.hasMany(models.Users_Events, { foreignKey: 'event_id' });
+      Event.belongsToMany(models.User, {
+        through: models.Users_Events,
+        as: 'user_list',
+        foreignKey: 'event_id'
+      });
     }
   }
   Event.init(
--- models/user.js
+++ models/user.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Comment, { foreignKey: 'user_id' });
-      User.hasMany(models.Users_Events, { foreignKey: 'user_id' });
+      User.belongsToMany(models.Event, {
+        as: 'event_list',
+        through: models.Users_Events,
+        foreignKey: 'user_id'
+      });
     }
   }
   User.init(
