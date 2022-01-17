d422f96f6a00a053a9771d664154c4f377b1c0e4
janemzwangnj <janemzwangnj@gmail.com>
2022-01-17 15:56:29 -0500

    add join table routers and controller files

--- routes/UsersEventsRouter.js
+++ routes/UsersEventsRouter.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const controller = require('../controllers/usersEventsController');
+
+router.get('/', controller.getAllUsersEvents);
+router.post('/', controller.createNewUsersEvents);
+router.put('/:id', controller.updateUsersEvents);
+router.delete('/:id', controller.deleteUsersEvents);
 
 module.exports = router;
