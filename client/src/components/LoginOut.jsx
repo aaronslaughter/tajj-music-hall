91b7f41c449a48713e7f83df70f11b4e784d6d0f
julio <julior0518@gmail.com>
2022-01-18 04:48:38 -0500

    popUp complete needst props to work correctly

--- client/src/components/LoginOut.jsx
+++ client/src/components/LoginOut.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import e from 'cors';
+import { useRef, useState } from 'react'
 import LogIn from '../pages/LogIn';
 import Register from '../pages/Register';
 import '../styles/LogInOut.css'
 
-function LogInOut (){
+function LogInOut (props){
   const [form, setForm] = useState(false)
+  const close = useRef()
 
   function handleClick(){
     setForm(!form)
+  }
+
+  function closePop(e){
+    console.log(props)
+    if(close.current === e.target){ props.setPop(!props.pop)}
   }
 
   let display
@@ -16,7 +23,7 @@ function LogInOut (){
   form ? text=<p onClick={handleClick}>Need to create a new account</p>:text=<p onClick={handleClick}>Already have an account</p>
 
   return(
-  <div className="pop">
+  <div className="pop" ref={close} onClick={closePop}>
     <div className="popUpForm">
       {display}
       {text}
