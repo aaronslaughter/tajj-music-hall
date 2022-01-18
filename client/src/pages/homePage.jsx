bce8ac291048daa0d49102536cde24ed426915d0
julio <julior0518@gmail.com>
2022-01-18 03:00:47 -0500

    created homepage styling

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,8 +1,23 @@
+import pic1 from './landingPic.jpg'
+import BigLogo from './LogoLandingPage-8.png'
+import BrandText from './BrandText-8.png'
+import '../styles/HomePage.css'
+
 
 const homePage = () => {
   return (
-    <div>
-     what is this
+    <div className='homePage'>
+      <div className='Top'>
+        <img src={pic1} alt='Concert_picture' id="pic1"/>
+        <div className='TopOver'>
+          <img src={BigLogo} alt='BigLogo' id="BigLogo"/>
+          <div  id="BrandText">
+            <img src={BrandText} alt='BrandText'  id="BrandText"/>
+            <button className='actionButton'>Login</button>
+          </div>
+        </div>
+      </div>
+      <h1>FOLLOW YOUR FAVOURITE <br></br> STARS ON TOUR</h1>
     </div>
   )
 }
