a35bfdeebcde771904ae1afd4718b4d623ea2230
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-19 10:12:05 -0500

    setup for heroku deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const logger = require('morgan')
 const AuthRouter = require('./routes/AuthRouter')
 const AppRouter = require('./routes/AppRouter');
+const path = require('path')
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,4 +16,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.json({ message: 'Server Works' }));
 app.use('/api', AppRouter);
 app.use('/auth', AuthRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
