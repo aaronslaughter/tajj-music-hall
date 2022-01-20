4530199da345ce84b8cbd0dc31d4bcd84df2cbe2
Terra Smith <t_m@linuxmail.org>
2022-01-20 06:51:59 -0800

    modified splash page

--- client/src/LandingSplash.jsx
+++ client/src/LandingSplash.jsx
@@ -5,15 +5,12 @@ import TajjMuHall from '../src/videos/TajjMuHall.mov'
 
 export default function LandingSplash() {
 
-  //onclick go to homepage
 
   return (
 
     <div className='tajj_vid'>
-      <Link to='/homepage'><video autoPlay muted loop src={TajjMuHall} width="100%">
+      <Link to='/home'><video autoPlay muted loop src={TajjMuHall} width="100%">
       </video></Link>
     </div >
   )
 }
-
-// <Link to="/signup" className="btn btn-primary">Sign up</Link>
