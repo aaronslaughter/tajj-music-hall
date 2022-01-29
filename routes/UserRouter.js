fdbed12499f9dc3ff99610b46207d1f089a5c408
janemzwangnj <janemzwangnj@gmail.com>
2022-01-29 12:40:25 -0500

    updated userRouter.js

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,11 +1,42 @@
 const router = require('express').Router();
 const controller = require('../controllers/userController');
+const middleware = require('../middleware');
 
-router.get('/', controller.getAllUsers);
-router.get('/events/:userId', controller.getEventsByUserId);
-router.get('/events/name/:userName', controller.getEventsByUserName);
-router.post('/', controller.createNewUser);
-router.put('/:userId', controller.updateUser);
-router.delete('/:userId', controller.deleteUser);
+router.get(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getAllUsers
+);
+router.get(
+  '/events/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getEventsByUserId
+);
+router.get(
+  '/events/name/:userName',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getEventsByUserName
+);
+router.post(
+  '/',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.createNewUser
+);
+router.put(
+  '/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateUser
+);
+router.delete(
+  '/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteUser
+);
 
 module.exports = router;
